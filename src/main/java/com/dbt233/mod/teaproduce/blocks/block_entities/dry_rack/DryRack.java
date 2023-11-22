06b1892023dd8e2a68c63522e86aceeb2e46063b
TagsGroup <Expect_XD_@outlook.com>
2023-11-22 21:58:16 +0800

    DryRack 物品模型(手上+掉落物) 未修复

--- src/main/java/com/dbt233/mod/teaproduce/blocks/block_entities/dry_rack/DryRack.java
+++ src/main/java/com/dbt233/mod/teaproduce/blocks/block_entities/dry_rack/DryRack.java
@@ -3,8 +3,11 @@ package com.dbt233.mod.teaproduce.blocks.block_entities.dry_rack;
 import com.dbt233.mod.teaproduce.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.Container;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
@@ -19,6 +22,7 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
 
 public class DryRack extends BaseEntityBlock {
@@ -74,5 +78,20 @@ public class DryRack extends BaseEntityBlock {
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean b) {
+        if (!blockState.is(blockState2.getBlock())) {
+            BlockEntity blockEntity = level.getBlockEntity(blockPos);
+            if (blockEntity instanceof DryRackBlockEntity) {
+                ItemStackHandler inventory = ((DryRackBlockEntity) blockEntity).getInventory();
+                for (int slot = 0; slot < inventory.getSlots(); slot++) {
+                    level.addFreshEntity(new ItemEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), inventory.getStackInSlot(slot)));
+                }
+            }
+            level.updateNeighbourForOutputSignal(blockPos, this);
+        }
+        super.onRemove(blockState, level, blockPos, blockState2, b);
     }
 }
